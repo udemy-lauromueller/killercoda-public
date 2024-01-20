c793e44de0091cd06c17bf5df5924059993801e8
Lauro Fialho Müller <contact@lauromueller.com>
2024-01-20 22:15:37 +0100

    Update install-aws.sh

--- install-aws.sh
+++ install-aws.sh
@@ -1,5 +1,5 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
-unzip awscliv2.zip
+unzip awscliv2.zip > /dev/null 2>&1
 
-sudo ./aws/install
+sudo ./aws/install > /dev/null 2>&1
